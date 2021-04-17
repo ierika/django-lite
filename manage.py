0e7e600e1dd68ff4cb096cef5348c9341184888f
Erika Suzuki <haloerika@gmail.com>
2021-04-18 04:13:17 +0900

    Fix a misplaced line of code

--- manage.py
+++ manage.py
@@ -39,12 +39,10 @@ def index(_):
 
 # Routes
 urlpatterns = [
-    path('', index),
+    path('', index, name='index'),
 ]
 
 
 if __name__ == "__main__":
     from django.core.management import execute_from_command_line
-
-
-execute_from_command_line(sys.argv)
+    execute_from_command_line(sys.argv)
